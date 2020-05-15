a0b262a2d99537f4e7800af1f239cf3603f6ca3c
ruizhou <838567391@qq.com>
2020-05-15 22:14:30 +0800

    开发注册功能以及链接激活

---
+++ src/main/java/com/spring/community2/util/CommunityConstant.java
@@ -0,0 +1,18 @@
+package com.spring.community2.util;
+
+public interface CommunityConstant {
+    /**
+     * 激活成功
+     */
+    int ACTIVATION_SUCCESS = 0;
+
+    /**
+     * 重复激活
+     */
+    int ACTIVATION_REPATE = 1;
+
+    /**
+     * 激活失败
+     */
+    int ACTIVATION_FAILURE = 2;
+}
