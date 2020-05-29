9831aa45fa59843b217ecb51f1f591756711d709
ruizhou <838567391@qq.com>
2020-05-29 16:20:44 +0800

    显示系统通知

--- src/main/java/com/spring/community2/config/WebMvcConfig.java
+++ src/main/java/com/spring/community2/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.spring.community2.config;
 
 import com.spring.community2.controller.interceptor.LoginRequiredInterceptor;
 import com.spring.community2.controller.interceptor.LoginTicketInterceptor;
+import com.spring.community2.controller.interceptor.MessageInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -20,12 +21,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginRequiredInterceptor loginRequiredInterceptor;
 
+    @Autowired
+    private MessageInterceptor messageInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginTicketInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 
         registry.addInterceptor(loginRequiredInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+        registry.addInterceptor(messageInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
     }
 }
---
+++ src/main/java/com/spring/community2/controller/interceptor/MessageInterceptor.java
@@ -0,0 +1,36 @@
+package com.spring.community2.controller.interceptor;
+
+import com.spring.community2.entity.User;
+import com.spring.community2.service.MessageService;
+import com.spring.community2.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @ClassName MessageInterceptor
+ * @Author ruizhou
+ * @Date 2020/5/29 16:16
+ **/
+@Component
+public class MessageInterceptor implements HandlerInterceptor {
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private MessageService messageService;
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        User user = hostHolder.getUser();
+        if (user != null && modelAndView != null) {
+            int letterUnreadCount = messageService.findLetterUnreadCount(user.getId(), null);
+            int noticeUnreadCount = messageService.findNoticeUnreadCount(user.getId(), null);
+            modelAndView.addObject("allUnreadCount", letterUnreadCount + noticeUnreadCount);
+        }
+    }
+}
